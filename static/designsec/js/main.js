7c67db1654bca9fce7dc79c87d115973954e664a
arewm <ajmcnama@ncsu.edu>
2017-07-21 13:54:34 -0700

    move glyph changes to css

--- static/designsec/js/main.js
+++ static/designsec/js/main.js
@@ -1,9 +1,3 @@
-function toggleIcon(e) {
-    $(e.target)
-        .prev('.panel-heading')
-        .find('.more-less')
-        .toggleClass('glyphicon-plus glyphicon-minus');
-}
 
 $(document).ready(function () {
     var listLoader = $('#list-maker');
@@ -13,7 +7,7 @@ $(document).ready(function () {
         data: listLoader.serialize(),
         success: function (data) {
                 $('#recommendations').html(data);
-                $('#category-sorters').children().first().addClass('active')
+                $('#category-sorters').children().first().addClass('active');
         },
         error: function (data) {
                 alert("Something went wrong!" + data);
@@ -34,16 +28,10 @@ $(document).ready(function () {
                 $('#recommendations').html(data);
                 $('#category-sorters').find('li.active').removeClass('active');
                 selection.addClass('active');
-
-                // register the new show/hide clicks
-                $('.panel-group').on('hide.bs.collapse', toggleIcon);
-                $('.panel-group').on('show.bs.collapse', toggleIcon);
             },
             error: function (data) {
                 alert("Something went wrong!" + data);
             }
         });
     });
-
-    // todo make a function to roll up/down each field
 });
